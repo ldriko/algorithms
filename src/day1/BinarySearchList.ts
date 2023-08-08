7c87b8dfa05d8e124ba5e10b7df54a89c26a989e
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-08-09 03:12:02 +0700

    feat: add binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    function halveAndFind(lo: number, hi: number): boolean {
+        let m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
 
+        if (v === needle) {
+            return true;
+        } else if (hi <= lo) {
+            return false;
+        } else if (needle > v) {
+            lo = m + 1;
+        } else {
+            hi = m;
+        }
+
+        return halveAndFind(lo, hi);
+    }
+
+    return halveAndFind(0, haystack.length);
 }
