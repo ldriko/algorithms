e8c2b6e89bbba58a275b05b7f5d163001b73fb1e
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-08-28 03:07:14 +0700

    feat: add two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = 0;
 
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) break;
+    }
+
+    i = i - jumpAmount;
+
+    for (let j = i; j <= i + jumpAmount; j++) {
+        if (breaks[j]) return j;
+    }
+
+    return -1;
 }
