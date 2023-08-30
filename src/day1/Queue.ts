2adabefe482e6831188cb394e9170178a9991a88
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-08-30 23:28:50 +0700

    feat: add queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { val: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) return undefined;
 
-}
+        this.length--;
+
+        const h = this.head;
+        this.head = this.head.next;
+        h.next = undefined;
+
+        if (this.length === 0) this.tail = undefined;
+
+        return h.val;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
